ce26d40d45bc793821caeb7b079eaf0dab785e2e
Rickard Lindberg <rickard@rickardlindberg.me>
2023-07-16 07:03:02 +0200

    New Event class.

--- rlvideolib/domain/project.py
+++ rlvideolib/domain/project.py
@@ -9,6 +9,28 @@ from rlvideolib.domain.source import FileSource
 from rlvideolib.domain.source import Sources
 from rlvideolib.domain.source import TextSource
 
+class Event:
+
+    """
+    >>> def listener():
+    ...     print("got event")
+    >>> event = Event()
+    >>> event.trigger()
+    >>> event.listen(listener)
+    >>> event.trigger()
+    got event
+    """
+
+    def __init__(self):
+        self.listeners = []
+
+    def trigger(self):
+        for fn in self.listeners:
+            fn()
+
+    def listen(self, fn):
+        self.listeners.append(fn)
+
 class Project:
 
     # TODO: make observable and GUI listens for change events
