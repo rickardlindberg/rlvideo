3b212267541ce379e6ca247b85f30948ea136512
Rickard Lindberg <rickard@rickardlindberg.me>
2023-07-21 21:18:10 +0200

    Spike mlt aac export.

--- spikes/mlt_hello_world.py
+++ spikes/mlt_hello_world.py
@@ -90,6 +90,50 @@ def hello6():
     while consumer.is_stopped() == 0:
         time.sleep(1)
 
+def export_aac_blank_test():
+    mlt.Factory().init()
+    profile = mlt.Profile()
+    playlist = mlt.Playlist()
+    playlist.append(mlt.Producer(profile, "color:red"), 0, 25)
+    playlist.blank(25)
+    playlist.append(mlt.Producer(profile, "color:green"), 0, 25)
+    #playlist.attach(mlt.Filter(profile, "audioconvert"))
+
+    #consumer = mlt.Consumer(profile, "xml")
+    #consumer.connect(playlist)
+    #consumer.start()
+    #while consumer.is_stopped() == 0:
+    #    time.sleep(1)
+
+    playlist = mlt.Producer(playlist)
+
+    consumer = mlt.Consumer(profile, "avformat")
+    consumer.set("target", "export.mp4")
+    consumer.set("acodec", "aac")
+    consumer.connect(playlist)
+    consumer.start()
+    while consumer.is_stopped() == 0:
+        time.sleep(1)
+
+def export_aac_blank_test2():
+    mlt.Factory().init()
+    profile = mlt.Profile()
+    producer = mlt.Producer(profile, "test.xml")
+    debug_producer(producer)
+    consumer = mlt.Consumer(profile, "avformat")
+    consumer.set("target", "export.mp4")
+    consumer.set("acodec", "aac")
+    consumer.connect(producer)
+    consumer.start()
+    while consumer.is_stopped() == 0:
+        time.sleep(1)
+
+def debug_producer(producer, indent=0):
+    print(f"{'  '*indent}Producer: {producer.debug()}")
+    for i in range(producer.filter_count()):
+        f = producer.filter(i)
+        print(f"{'  '*indent}Filter: {f.debug()}")
+
 if __name__ == "__main__":
-    hello4()
+    export_aac_blank_test()
     print("OK")
