dda14cf7fba656e7651270edc96471844b0c7bef
Rickard Lindberg <rickard@rickardlindberg.me>
2023-07-31 06:55:45 +0200

    Remove unused import of FileInfo.

--- rlvideolib/domain/source.py
+++ rlvideolib/domain/source.py
@@ -10,7 +10,6 @@ from rlvideolib.domain.clip import Clip
 from rlvideolib.domain.cut import Cut
 from rlvideolib.domain.cut import CutSource
 from rlvideolib.domain.region import Region
-from rlvideolib.mlthelpers import FileInfo
 from rlvideolib.testing import capture_stdout_stderr
 
 class FileSource(namedtuple("FileSource", "id,path,number_of_frames_at_project_fps")):
